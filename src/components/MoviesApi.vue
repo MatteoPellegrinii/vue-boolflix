44e962413b8676486294e96322d489447ee9c914
MatteoPellegrinii <pellegrini.matteo00@gmail.com>
2022-06-15 16:48:34 +0200

    push

--- src/components/MoviesApi.vue
+++ src/components/MoviesApi.vue
@@ -1,6 +1,8 @@
 <template>
 <div>
     <h2>{{movieobject.title}} {{movieobject.name}} </h2>
+    <h4>lingua: {{movieobject.original_language}} </h4>
+    <h5>voto {{votechange()}} </h5>
     <img :src="`http://image.tmdb.org/t/p/w500/${movieobject.poster_path}`" :alt="name">
 </div>
  
@@ -8,11 +10,22 @@
 
 <script>
 export default {
+  data(){
+    return{
+      votonuovo: '',
+    }
+  },
   name: 'MoviesAPi',
       props: {
         name: String,
         src: String,
         movieobject: Object
+    },
+    methods:{
+      votechange(){
+        this.votonuovo = Math.floor(this.movieobject.vote_average / 2) ;
+        return this.votonuovo
+      }
     }
 }
 </script>
